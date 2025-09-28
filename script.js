f1a3718437e91b115a52be4bfb2e7400b556cdf4
Satyendra Kumar Lokesh <99381141+Satyendra2122@users.noreply.github.com>
2025-09-28 19:13:24 +0530

    Update v2.30

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 document.getElementById("toggle-mode").addEventListener("click", function () {
-    const button = this;
     const body = document.body;
-    const header = document.getElementById("header");
-    const heroSection = document.getElementById("hero-section");
-    const backgroundImage = document.getElementById("background-image");
+    const button = this;
 
-    if (button.textContent === "DARKMODE") {
-        // Apply dark mode styles
-        body.classList.add("dark-mode");
+    if (body.classList.contains("light-mode")) {
         body.classList.remove("light-mode");
-        header.classList.add("dark-header");
-        header.classList.remove("light-header");
-        heroSection.classList.add("dark-hero");
-        heroSection.classList.remove("light-hero");
-        backgroundImage.src = "./img/bg-img2-dark.jpg"; // Change to dark mode background image
+        body.classList.add("dark-mode");
         button.textContent = "LIGHTMODE";
     } else {
-        // Apply light mode styles
-        body.classList.add("light-mode");
         body.classList.remove("dark-mode");
-        header.classList.add("light-header");
-        header.classList.remove("dark-header");
-        heroSection.classList.add("light-hero");
-        heroSection.classList.remove("dark-hero");
-        backgroundImage.src = "./img/bg-img4-light.jpg"; // Change to light mode background image
+        body.classList.add("light-mode");
         button.textContent = "DARKMODE";
     }
 });
